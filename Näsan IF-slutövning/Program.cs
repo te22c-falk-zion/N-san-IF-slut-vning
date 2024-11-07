c486907e482d838e70d743d927ec4840404440ca
Zion Falk <zion.falk@elev.ga.ntig.se>
2024-11-07 10:32:38 +0100

    first commit

---
+++ Näsan IF-slutövning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+
+int randomNumber = Random.Shared.Next(0, 10);
+string guess = "";
+int guessNumber;
+
+
+while (guess != randomNumber.ToString())
+{
+    Console.WriteLine("Guess the number 1-10");
+    guess = Console.ReadLine();
+
+    int.TryParse(guess, out guessNumber);
+    if (guessNumber > randomNumber)
+    {
+        Console.WriteLine("Lower!");
+    }
+    if (guessNumber < randomNumber)
+    {
+        Console.WriteLine("Higher!");
+    }
+    else if (guessNumber == randomNumber)
+    {
+        Console.WriteLine("Congrats you got it right!");
+        Console.ReadLine();
+    }
+}
